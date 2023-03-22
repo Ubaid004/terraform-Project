ccc168b0014e19a5a96f3ac30d699a94e593245c
UBAID KHAN <75469619+Ubaid004@users.noreply.github.com>
2023-03-22 23:07:23 +0530

    new

---
+++ instance.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "instance" {
+
+        ami = "ami-05afd67c4a44cc983"
+        instance_type = "t2.micro"
+        vpc_security_group_ids =[aws_security_group.terra-SG.id]
+        iam_instance_profile=aws_iam_role.terra-role.name
+        user_data= <<EOF
+
+        #!/bin/bash
+        sudo sh
+        sudo apt-get update -y
+        sudo apt-get install nginx -y
+
+        aws s3 cp s3://${aws_s3_bucket.my-s3-bucket.id}/index.html  /var/www/html/index.html
+
+        sudo systemctl start nginx
+        sudo systemctl enable nginx
+EOF
+        tags = {
+              Name = "Terraweb"
+}
+}
