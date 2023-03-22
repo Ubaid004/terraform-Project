2c175dd7854aa19e2b52e0f098b5ff60db64e1bf
UBAID KHAN <75469619+Ubaid004@users.noreply.github.com>
2023-03-22 23:08:43 +0530

    new

--- terra-iam.tf
+++ terra-iam.tf
@@ -1,33 +1,33 @@
-resource "aws_iam_role" "terra"  {
-        name= "terra"
-        assume_role_policy= <<EOF
-{
+resource "aws_iam_role" "terra-role" {
+        name = "terra-role"
+        assume_role_policy =jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy" "terra-policy" {
+        name = "terra-policy"
+        role = aws_iam_role.terra-role.name
+        policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
         {
             "Effect": "Allow",
-            "Principal": {
-                "Service": "ec2.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
+            "Action": "*",
+            "Resource": "*"
         }
     ]
+})
 }
 
-EOF
-}
-
-
-resource "aws_iam_role_s3" "terra"{
-        name= "terra"
-        role= aws_iam_role.terra.name
-}
-
-resource "aws_iam_policy_Attached" "role-plicy-attached"{
-        for_each = toset([
-        "arn:aws:iam::600595126159:instance-profile/terrafrom",
-        "arn:aws:iam::600595126159:role/terrafrom"
-])
-        role= aws_iam_role.terra.name
-        policy_arn= each.value
-}
