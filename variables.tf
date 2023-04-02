a6e8e99a94b954a52bc8eb1513589493149fb391
UBAID KHAN <75469619+Ubaid004@users.noreply.github.com>
2023-04-02 14:24:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,26 @@
+# -------------------------------------------
+# Common Variables
+# -------------------------------------------
+
 variable "aws_region" {
-  description = "The AWS region to use to create resources."
+  description = "AWS infrastructure regio"
+  type        = string
   default     = "ap-south-1"
 }
+
+variable "tags" {
+  description = "Tag map for the resource"
+  type        = map(string)
+  default     = {}
+}
+
+
+
+
+# -------------------------------------------
+# bucket Variables
+# -------------------------------------------
+
 
 variable "bucket_prefix" {
     type        = string
@@ -9,7 +28,7 @@ variable "bucket_prefix" {
     default     = "my-s3bucket-"
 }
 
-variable "tags" {
+variable "tags_bt" {
     type        = map
     description = "(Optional) A mapping of tags to assign to the bucket."
     default     = {
@@ -27,96 +46,189 @@ variable "versioning" {
 variable "acl" {
     type        = string
     description = " Defaults to private "
-    default     = "private"  
+    default     = "private"
 }
 
-# Load Balancer
 
-variable "lb_protocol" {
-        description = "lb_protocol HTTP (ALB) or TCP (NLB)"
-        type        = string
-        default     = "HTTP"
+# -------------------------------------------
+# VPC Variables
+# -------------------------------------------
+
+variable "create_vpc" {
+  description = "decision to create VPC"
+  type        = bool
+  default     = true
 }
 
-variable "lb_listener_protocol" {
-        description = "lb_listener_protocol HTTP, TCP, TLS"
-        type        = string
-        default     = "HTTP"
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = "terra-vpc"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  type        = string
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+
+# -------------------------------------------
+# Internet Gateway Variables
+#-------------------------------------------
+
+variable "create_igw" {
+  description = "decision to create IGW"
+  type        = bool
+  default     = false
+}
+
+
+# -------------------------------------------
+# Security Group
+# -------------------------------------------
+
+variable "sg_name" {
+  description = "sg_name"
+  type        = string
+  default     = "terra-sg"
+}
+
+variable "app_port" {
+  description = "app_port"
+  type        = number
+  default     = 443
+}
+
+variable "aws_vpc_id" {
+  description = "aws_vpc_id"
+  type        = string
+  default     = ""
+}
+
+variable "allow_all_ips" {
+  description = "allow_all_ips"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "aws_vpc_main_cidr_block" {
+  description = "aws_vpc_main_cidr_block"
+  type        = list(string)
+  default     = []
+}
+# -------------------------------------------
+# Loab Balancer
+# -------------------------------------------
+
+variable "lb_name" {
+  description = "LB name"
+  type        = string
+  default     = "Mylb"
+}
+
+variable "lb_internal" {
+  description = "Internal true or false"
+  type        = bool
+  default     = false
+}
+
+variable "lb_load_balancer_type" {
+  description = "Application or Network type LB"
+  type        = string
+  default     = "application"
+}
+
+variable "lb_security_groups" {
+  description = "LB security groups"
+  type        = list(string)
+  default     = ["sg-07b9d22bfeeb2bbd6"]
+}
+
+variable "lb_subnets" {
+  description = "LB subnets"
+  type        = list(string)
+  default     = ["subnet-08efb067b0c124552", "subnet-0628aae2213d66d4a"]
 }
 
 variable "lb_enable_deletion_protection" {
-        description = "enable_deletion_protection true or false"
-        type        = bool
-        default     = false
-}
-
-variable "vpc_cidr" {
-        type= string
-        default= "10.0.0.0/16"
-}
-
-variable "vpc_name"{
-        type= string
-        default= "terra-vpc1"
-}
-
-variable "lb_name"{
-        type= string
-        default = "terra-lb"
-}
-
-variable "lb_internal"{
-        type= bool
-        default= "false"
+  description = "enable_deletion_protection true or false"
+  type        = bool
+  default     = false
 }
 
 
-variable "lb_load_balancer_type"{
-        type= string
-        default= "application"
+variable "lb_target_port" {
+  description = "lb_target_port 80 or 443"
+  type        = number
+  default     = 80
 }
 
-variable "lb_subnets"{
-        type= list(string)
-        default = ["subnet-08efb067b0c124552","subnet-0628aae2213d66d4a"]
+variable "lb_protocol" {
+  description = "lb_protocol HTTP (ALB) or TCP (NLB)"
+  type        = string
+  default     = "HTTP"
 }
+
+variable "lb_target_type" {
+  description = "Target type ip (ALB/NLB), instance (Autosaling group)"
+  type        = string
+  default     = "instance"
+}
+
 variable "lb_vpc_id" {
-        description = "vpc_id"
-        type        = string
-        default     = null
-}
-
-
-variable "lb_target_type"{
-        type= string
-        default = "instance"
-}
-
-variable "lb_target_port"{
-        type = number
-        default = "80"
+  description = "vpc_id"
+  type        = string
+  default     = null
 }
 
 variable "lb_listener_port" {
-        description = "lb_listener_port"variable "lb_tags" {
-        description = "Tag map for the resource"
-        type        = map(string)
-        default     = {}
+  description = "lb_listener_port"
+  type        = number
+  default     = 80
 }
+
+variable "lb_listener_protocol" {
+  description = "lb_listener_protocol HTTP, TCP, TLS"
+  type        = string
+  default     = "HTTP"
+}
+
+
 
 variable "lb_target_tags_map" {
-        description = "Tag map for the LB target resources"
-        type        = map(string)
-        default     = {}
+  description = "Tag map for the LB target resources"
+  type        = map(string)
+  default     = {}
 }
 
-variable "lb_tags" {
-        description = "Tag map for the resource"
-        type        = map(string)
-        default     = {}
-}
+# -------------------------------------------
+# Auto scalling
+# -------------------------------------------
 
-# Auto Scalling
+variable "as_name" {
+  description = "AS name"
+  type        = string
+  default     = "Myas"
+}
 
 variable "max_size"{
         default = "1"
@@ -134,7 +246,4 @@ variable "target_group_arns"{
         default = []
 }
 
-                                                                                                                                                                  145,0-1       Bot
 
-
- 
