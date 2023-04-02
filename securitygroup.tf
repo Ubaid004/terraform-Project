7afc4f9247ab42e8516109158dcb10e9d17c320b
UBAID KHAN <75469619+Ubaid004@users.noreply.github.com>
2023-04-02 14:27:22 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,27 +1,26 @@
-resource "aws_security_group" "terra-SG"{
-        name = "terra-SG"
+resource "aws_security_group" "terra" {
+  name        = var.sg_name
+  description = "Security group for ${var.aws_vpc_id}"
+  vpc_id      = var.aws_vpc_id
 
-        ingress {
-                from_port= 80
-                to_port= 80
-                protocol= "tcp"
-                cidr_blocks= ["0.0.0.0/0"]
+  ingress {
+    description = "Ingress from VPC ${var.aws_vpc_id}"
+    from_port   = var.app_port #443
+    to_port     = var.app_port #443
+    protocol    = "tcp"
+    cidr_blocks = var.aws_vpc_main_cidr_block #[aws_vpc.main.cidr_block]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
 
-        }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"                # allow all outbound
+    cidr_blocks = [var.allow_all_ips] #["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
 
-         ingress {
-                from_port= 443
-                to_port= 443
-                protocol= "tcp"
-                cidr_blocks= ["0.0.0.0/0"]
-
-        }
-
-         egress {
-                from_port= 0
-                to_port= 0
-                protocol= "-1"
-                cidr_blocks= ["0.0.0.0/0"]
-
-        }
+  tags = merge(
+    { "ResourceName" = var.sg_name }, var.tags
+  )
 }
